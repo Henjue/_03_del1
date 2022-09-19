ff701ffce8a371e1664f825873ec76683a3f28ca
Lars Siegvardsen <s224287@student.dtu.dk>
2022-09-19 16:06:17 +0200

    Test branch lavet og indsat Random gen

--- src/com/cdio/spil/Main.java
+++ src/com/cdio/spil/Main.java
@@ -1,5 +1,10 @@
 package com.cdio.spil;
+import java.util.Random;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Random random = new Random();
+        int x = random.nextInt(6) + 1;
+        System.out.println(x);
+    }
 }
