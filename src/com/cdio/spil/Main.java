7bf08b8f41f6c6afa25eef02c2d7c1f3f02ec683
Lars Siegvardsen <s224287@student.dtu.dk>
2022-09-19 16:36:55 +0200

    Lavet 2 players

--- src/com/cdio/spil/Main.java
+++ src/com/cdio/spil/Main.java
@@ -3,10 +3,18 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        int x, y, player_1 = 0, player_2 = 0;
         Random random = new Random();
-        int x = random.nextInt(6) + 1;
-        int y = random.nextInt(6) + 1;
+        x = random.nextInt(6) + 1;
+        y = random.nextInt(6) + 1;
         System.out.println("Dice One: " + x + "\n" + "Dice Two: " + y);
+        player_1 = player_1+x+y;
+        System.out.println(player_1);
+        x = random.nextInt(6) + 1;
+        y = random.nextInt(6) + 1;
+        System.out.println("Dice One: " + x + "\n" + "Dice Two: " + y);
+        player_2 = player_2+x+y;
+        System.out.println(player_2);
         //test
     }
 }
