fd6853a140733601ed9a324b22e23cb68a7f1166
Anton Helsgaun <s224268@student.dtu.dk>
2022-09-19 16:52:17 +0200

    Added new branch "loop"

--- src/com/cdio/spil/Main.java
+++ src/com/cdio/spil/Main.java
@@ -4,17 +4,22 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         int x, y, player_1 = 0, player_2 = 0;
-        Random random = new Random();
-        x = random.nextInt(6) + 1;
-        y = random.nextInt(6) + 1;
-        System.out.println("Dice One: " + x + "\n" + "Dice Two: " + y);
-        player_1 = player_1+x+y;
-        System.out.println(player_1);
-        x = random.nextInt(6) + 1;
-        y = random.nextInt(6) + 1;
-        System.out.println("Dice One: " + x + "\n" + "Dice Two: " + y);
-        player_2 = player_2+x+y;
-        System.out.println(player_2);
-        //yeet
+        while(player_1 < 40 && player_2 < 40) {
+            Random random = new Random();
+            x = random.nextInt(6) + 1;
+            y = random.nextInt(6) + 1;
+            System.out.println("Dice One: " + x + "\n" + "Dice Two: " + y);
+            player_1 = player_1 + x + y;
+            System.out.println("Player 1 score is " + player_1);
+            x = random.nextInt(6) + 1;
+            y = random.nextInt(6) + 1;
+            System.out.println("Dice One: " + x + "\n" + "Dice Two: " + y);
+            player_2 = player_2 + x + y;
+            System.out.println("Player 1 score is " + player_2);
+        }
+        if (player_1 > 39)
+            System.out.printf("Player 1 wins with " + player_1 + " points," + (player_1-player_2) + " ahead of player 2");
+        else
+            System.out.printf("Player 2 wins with " + player_2 + " points," + (player_2-player_1) + " ahead of player 1");
     }
 }
