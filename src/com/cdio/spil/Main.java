80fc1a5bfbefcb3821bf53cb5e237b427d02a616
Lars Siegvardsen <s224287@student.dtu.dk>
2022-09-19 17:13:05 +0200

    Lavet helt loop

--- src/com/cdio/spil/Main.java
+++ src/com/cdio/spil/Main.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         int x, y, player_1 = 0, player_2 = 0;
-        while(player_1 < 40 && player_2 < 40) {
+        while(true) {
             Random random = new Random();
             x = random.nextInt(6) + 1;
             y = random.nextInt(6) + 1;
@@ -15,12 +15,22 @@ public class Main {
             y = random.nextInt(6) + 1;
             System.out.println("Dice One: " + x + "\n" + "Dice Two: " + y);
             player_2 = player_2 + x + y;
-            System.out.println("Player 1 score is " + player_2);
-        }
-        if (player_1 > 39)
-            System.out.printf("Player 1 wins with " + player_1 + " points," + (player_1-player_2) + " ahead of player 2");
-        else
-            System.out.printf("Player 2 wins with " + player_2 + " points," + (player_2-player_1) + " ahead of player 1");
+            System.out.println("Player 2 score is " + player_2);
+
+            if (player_1 >=40 && player_1 == player_2){
+                System.out.println("Uafgjort");
+                break;
+            }
+            else if (player_1 >= 40)
+            {
+                System.out.printf("Player 1 wins with " + player_1 + " points," + (player_1 - player_2) + " ahead of player 2");
+                break;
+            }
+            else if (player_2 >=40){
+                System.out.printf("Player 2 wins with " + player_2 + " points," + (player_2 - player_1) + " ahead of player 1");
+                break;
+            }
         //hey there
+        }
     }
 }
