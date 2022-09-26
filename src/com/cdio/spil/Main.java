4e0aeb557622c0296c6ea6e836b18f10815821f8
Lars Siegvardsen <s224287@student.dtu.dk>
2022-09-26 15:04:40 +0200

    Lavet pause i mellem hvert slag.

--- src/com/cdio/spil/Main.java
+++ src/com/cdio/spil/Main.java
@@ -1,16 +1,24 @@
 package com.cdio.spil;
 import java.util.Random;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int x, y, player_1 = 0, player_2 = 0;
+        String b;
+        Scanner spil;
         while(true) {
             Random random = new Random();
+            spil = new Scanner(System.in);
+            System.out.println("Player 1, press enter to roll the die");
+            b = spil.nextLine();
             x = random.nextInt(6) + 1;
             y = random.nextInt(6) + 1;
             System.out.println("Dice One: " + x + "\n" + "Dice Two: " + y);
             player_1 = player_1 + x + y;
             System.out.println("Player 1 score is " + player_1);
+            spil = new Scanner(System.in);
+            System.out.println("Player 2, press enter to roll the die");
+            b = spil.nextLine();
             x = random.nextInt(6) + 1;
             y = random.nextInt(6) + 1;
             System.out.println("Dice One: " + x + "\n" + "Dice Two: " + y);
@@ -32,7 +40,6 @@ public class Main {
                 System.out.printf("Player 2 wins with " + player_2 + " points," + (player_2 - player_1) + " ahead of player 1");
                 break;
             }
-            //Done
         }
     }
 }
