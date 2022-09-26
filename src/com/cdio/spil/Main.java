4cfd60ade320329e4b05b5f01a6d70a50806db1f
Lucas Meldgaard <luucmeldgaard@utlook.com>
2022-09-26 15:31:49 +0200

    Conditions have been added

--- src/com/cdio/spil/Main.java
+++ src/com/cdio/spil/Main.java
@@ -1,18 +1,52 @@
 package com.cdio.spil;
 import java.util.Random;
+// Temporary test Scanner
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Test scanner
+        Scanner scanner = new Scanner(System.in);
         int x, y, player_1 = 0, player_2 = 0;
         while(true) {
             Random random = new Random();
             x = random.nextInt(6) + 1;
             y = random.nextInt(6) + 1;
+            if (x==1 && y==1) {
+                System.out.println("Player 1 has rolled two " + x + "'s\n and loses all points");
+                System.out.println("Press enter to continue...");
+                scanner.nextLine();
+
+            }
             System.out.println("Dice One: " + x + "\n" + "Dice Two: " + y);
             player_1 = player_1 + x + y;
             System.out.println("Player 1 score is " + player_1);
             x = random.nextInt(6) + 1;
             y = random.nextInt(6) + 1;
+            if (x==1 && y==1) {
+                System.out.println("Player 1 has rolled two " + x + "'s\n and loses all points");
+                System.out.println("Press enter to continue...");
+                player_1 = 0;
+                scanner.nextLine();
+            }
+            else if (x==y) {
+                boolean lastThrowSix = false;
+                System.out.println("Player 1 has rolled two " + x + "'s\n and may roll again");
+                System.out.println("Press enter to continue...");
+                scanner.nextLine();
+                if (x==6 && y==6) {
+                    lastThrowSix = true;
+                }
+                x = random.nextInt(6) + 1;
+                y = random.nextInt(6) + 1;
+                if (x==y && lastThrowSix) {
+                    System.out.println("Player 1 rolled two " + x + "and has won the game!");
+                    break;
+                }
+                player_1 = player_1 + x + y;
+
+            }
+
             System.out.println("Dice One: " + x + "\n" + "Dice Two: " + y);
             player_2 = player_2 + x + y;
             System.out.println("Player 2 score is " + player_2);
