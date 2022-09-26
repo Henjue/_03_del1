b3e773bfed8515ec79f969a6246b421fc5c826c8
Lars Siegvardsen <s224287@student.dtu.dk>
2022-09-26 17:01:21 +0200

    Lavet condition to ettere.

--- src/com/cdio/spil/Main.java
+++ src/com/cdio/spil/Main.java
@@ -9,21 +9,33 @@ public class Main  {
         while(true) {
             Random random = new Random();
             spil = new Scanner(System.in);
-            System.out.println("Player 1, press enter to roll the die");
+            System.out.println("\n" + "Player 1, press enter to roll the die");
             b = spil.nextLine();
             x = random.nextInt(6) + 1;
             y = random.nextInt(6) + 1;
             System.out.println("Dice One: " + x + "\n" + "Dice Two: " + y);
             player_1 = player_1 + x + y;
             System.out.println("Player 1 score is " + player_1);
+            if (x==1 && y==1) {
+                System.out.println("Player 1 has rolled two " + x + "'s\n and loses all points");
+                System.out.println("Press enter to continue...");
+                player_1 = 0;
+                System.out.println("\n" + "Player 1 score is " + player_1);
+            }
             spil = new Scanner(System.in);
-            System.out.println("Player 2, press enter to roll the die");
+            System.out.println("\n" + "Player 2, press enter to roll the die");
             b = spil.nextLine();
             x = random.nextInt(6) + 1;
             y = random.nextInt(6) + 1;
             System.out.println("Dice One: " + x + "\n" + "Dice Two: " + y);
             player_2 = player_2 + x + y;
             System.out.println("Player 2 score is " + player_2);
+            if (x==1 && y==1) {
+                System.out.println("Player 2 has rolled two " + x + "'s\n and loses all points");
+                System.out.println("Press enter to continue...");
+                player_2 = 0;
+                System.out.println("\n" + "Player 2 score is " + player_2);
+            }
 
             if (player_1 >=40 && player_1 == player_2)
             {
