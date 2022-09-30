18836c84d3639b63d27d2d67bec2d05507d5d6da
Lars Siegvardsen <s224287@Student.dtu.dk>
2022-09-30 14:59:49 +0200

    Basegame 3 done, Condition: Hvis man slår to seksere to gange i streg så vinder man spillet.

--- src/com/cdio/spil/Main.java
+++ src/com/cdio/spil/Main.java
@@ -22,7 +22,8 @@ public class Main {
                 System.out.println("Press enter to continue...");
                 player_1 = 0;
                 System.out.println("\n" + "Player 1 score is " + player_1);
-            } else if (x == y) {
+            }
+            else if (x == y && x!= 6) {
                 System.out.println("Player 1 has rolled two identical and therefore get another throw");
                 System.out.println("Press enter to continue...");
                 b = spil.nextLine();
@@ -33,7 +34,17 @@ public class Main {
                 System.out.println("Player 1 score is " + player_1);
                 spil = new Scanner(System.in);
             }
-
+            else if (x == y) {
+                System.out.println("Player 1 has rolled two sixes and therefore get another throw");
+                System.out.println("Press enter to continue...");
+                spil.nextLine();
+                x = random.nextInt(6) + 1;
+                y = random.nextInt(6) + 1;
+                if (x == y && x == 6){
+                    System.out.println("Player 1 has rolled double six twice in a row and wins the game");
+                    break;
+                }
+            }
             if (player_1 >= 40) {
                 System.out.println("Player 1 wins with " + player_1 + " points," + (player_1 - player_2) + " ahead of player 2");
                 break;
@@ -51,7 +62,7 @@ public class Main {
                 player_2 = 0;
                 System.out.println("\n" + "Player 2 score is " + player_2);
             }
-            else if (x == y) {
+            else if (x == y && x != 6) {
                 System.out.println("Player 2 has rolled two identical and therefore get another throw");
                 System.out.println("Press enter to continue...");
                 b = spil.nextLine();
@@ -61,6 +72,17 @@ public class Main {
                 player_2 = player_2 + x + y;
                 System.out.println("Player 2 score is " + player_2);
                 spil = new Scanner(System.in);
+            }
+            else if (x == y) {
+                System.out.println("Player 2 has rolled two sixes and therefore get another throw");
+                System.out.println("Press enter to continue...");
+                spil.nextLine();
+                x = random.nextInt(6) + 1;
+                y = random.nextInt(6) + 1;
+                if (x == y && x == 6){
+                    System.out.println("Player 2 has rolled double six twice in a row and wins the game");
+                    break;
+                }
             }
             if (player_2 >= 40) {
                 System.out.println("Player 2 wins with " + player_2 + " points," + (player_2 - player_1) + " ahead of player 1");
