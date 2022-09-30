9a38fb3e86c49def22c7a206f716ecf20b266749
Lars Siegvardsen <s224287@Student.dtu.dk>
2022-09-30 15:23:59 +0200

    Test af random class er opdateret.

--- src/com/cdio/spil/TestAfRandom.java
+++ src/com/cdio/spil/TestAfRandom.java
@@ -3,37 +3,39 @@ import java.util.Random;
 import java.util.Scanner;
 public class TestAfRandom {
     public static void main(String[] args) {
-        int i, runs, et = 0, to = 0, tre = 0, fire = 0, fem = 0, seks = 0, x, y;
+        int i, runs, to = 0, tre = 0, fire = 0, fem = 0, seks = 0, syv = 0, otte = 0, ni = 0, ti = 0, elleve = 0, tolv = 0, x, y;
         System.out.print("Indtast antal runs  "); //lader brugeren definere antal runs
-        System.out.println("(NB: Antal runs ganges med 2, da der er to terninger)");
+        System.out.println(" ");
         Scanner scan = new Scanner(System.in);
         runs = scan.nextInt();
         Random random = new Random();
         for (i = 0; i < runs; i++){
             x = random.nextInt(6) + 1;
             y = random.nextInt(6) + 1;
-            switch (x) { //tester for terning x
-                case 1 -> et++;
+            switch (x+y) {
                 case 2 -> to++;
                 case 3 -> tre++;
                 case 4 -> fire++;
                 case 5 -> fem++;
                 case 6 -> seks++;
-            } //tester for terning y
-            switch (y) {
-                case 1 -> et++;
-                case 2 -> to++;
-                case 3 -> tre++;
-                case 4 -> fire++;
-                case 5 -> fem++;
-                case 6 -> seks++;
+                case 7 -> syv++;
+                case 8 -> otte++;
+                case 9 -> ni++;
+                case 10 -> ti++;
+                case 11 -> elleve++;
+                case 12 -> tolv++;
             }
         } //Printer svarene
-        System.out.println("Total af etere = " + et);
-        System.out.println("Total af toere = " + to);
-        System.out.println("Total af treere = " + tre);
-        System.out.println("Total af firere = " + fire);
-        System.out.println("Total af femere = " + fem);
-        System.out.println("Total af seksere = " + seks);
+        System.out.println("Total sum to  = " + to);
+        System.out.println("Total sum tre = " + tre);
+        System.out.println("Total sum fire = " + fire);
+        System.out.println("Total sum fem = " + fem);
+        System.out.println("Total sum seks = " + seks);
+        System.out.println("Total sum syv = " + syv);
+        System.out.println("Total sum otte = " + otte);
+        System.out.println("Total sum ni = " + ni);
+        System.out.println("Total sum ti = " + ti);
+        System.out.println("Total sum elleve = " + elleve);
+        System.out.println("Total sum tolv = " + tolv);
     }
 }
