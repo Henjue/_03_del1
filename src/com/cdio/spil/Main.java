c48eb8fe5acdcc91a5e75496176825f33f8e3790
Lars Siegvardsen <s224287@Student.dtu.dk>
2022-09-30 14:10:08 +0200

    Basegame 1 done, condition: To ettere sletter alle point.

--- src/com/cdio/spil/Main.java
+++ src/com/cdio/spil/Main.java
@@ -1,6 +1,6 @@
 package com.cdio.spil;
-import java.util.Random;
-import java.util.Scanner;
+        import java.util.Random;
+        import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int x, y, player_1 = 0, player_2 = 0;
@@ -16,13 +16,18 @@ public class Main {
             System.out.println("Dice One: " + x + "\n" + "Dice Two: " + y);
             player_1 = player_1 + x + y;
             System.out.println("Player 1 score is " + player_1);
+            spil = new Scanner(System.in);
             if (x==1 && y==1) {
                 System.out.println("Player 1 has rolled two " + x + "'s\n and loses all points");
                 System.out.println("Press enter to continue...");
                 player_1 = 0;
                 System.out.println("\n" + "Player 1 score is " + player_1);
             }
-            spil = new Scanner(System.in);
+            if (player_1 >= 40)
+            {
+                System.out.println("Player 1 wins with " + player_1 + " points," + (player_1 - player_2) + " ahead of player 2");
+                break;
+            }
             System.out.println("\n" + "Player 2, press enter to roll the die");
             b = spil.nextLine();
             x = random.nextInt(6) + 1;
@@ -37,21 +42,10 @@ public class Main {
                 System.out.println("\n" + "Player 2 score is " + player_2);
             }
 
-            if (player_1 >=40 && player_1 == player_2)
+            if (player_2 >=40)
             {
-                System.out.println("Uafgjort");
+                System.out.println("Player 2 wins with " + player_2 + " points," + (player_2 - player_1) + " ahead of player 1");
                 break;
-            }
-            else if (player_1 >= 40 && player_2 < player_1)
-            {
-                System.out.printf("Player 1 wins with " + player_1 + " points," + (player_1 - player_2) + " ahead of player 2");
-                break;
-            }
-            else if (player_2 >=40)
-            {
-                System.out.printf("Player 2 wins with " + player_2 + " points," + (player_2 - player_1) + " ahead of player 1");
-                break;
-
             }
         }
     }
